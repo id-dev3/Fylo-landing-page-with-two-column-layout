b8ed6ab8a832ee9f004ef472413f467630b2633a
BlackFury117 <idrissadiallo97@icloud.com>
2023-03-22 14:07:22 +0000

    Fixing the hero section in desktop view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-    theme: {
+  theme: {
+    extend: {
       fontFamily: {
         'sans': ['Raleway', 'sans-serif'],
-        'secondSans': ['Open Sans', 'sans-serif'],
+        'main': ['"Open Sans"', 'sans-serif'],
       },
-      // ...
+      colors: {
+        'regal-blue': '#243c5a',
+        'custom-purple': 'hsl(243, 87%, 12%)',
+        'secondary': '#fff',
+      },
     },
-    // ...
-  }
+  },
+  variants: {},
+  plugins: [],
+};
